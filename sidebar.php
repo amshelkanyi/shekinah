c9e0cb8c127818bc23740fa4b68a5cfc98f2da19
hack3r <amshelkanyi@gmail.com>
2017-11-28 19:57:13 +0300

    sidebar code to display on all applicable pages

---
+++ sidebar.php
@@ -0,0 +1,80 @@
+<div class="grid-right">
+                    <div class="side-bar">
+                        <div class="grid-right-headings">
+                            <div class="animated swing"	/>
+                            <h2><a href="#">about shekinah</a></h2>
+                            <h6> </h6>
+                            <div class="clear"> </div>
+                        </div>
+                    </div>
+                    <p style="padding:0px;">Our vision is to be a christ centre Bible teaching, church dedicated to helping develop the spiritual growth of each member of the family and equip them to become active in  the ministry
+                    <div class="grid-right-headings">
+                        <div class="animated swing"/>
+                        <h2><a href="#">Area of Emphasis</a></h2>
+                        <h6> </h6>
+                        <div class="clear"> </div>
+                    </div>
+                </div>
+                <div class="seach-links">
+                    <p><a href="#">  Fellowship</a></p>
+                    <p><a href="#"> Discipleship</a></p>
+                    <p><a href="#"> Worship</a></p>
+                    <p><a href="#"> Ministry</a></p>
+                    <p><a href="#"> Evangelism</a></p>
+                </div>
+                <div class="grid-right-headings">
+                    <div class="animated swing"/>
+                    <h2>WEEKLY WORSHIP GUIDE</h2>
+                    <h6> </h6>
+                    <div class="clear"> </div>
+                </div>
+            </div>
+            <div class="gallery">
+                <table class="tg">
+                    <tr>
+                        <th class="tg-amwm">DAY</th>
+                        <th class="tg-amwm">EVENT</th>
+                        <th class="tg-amwm">TIME</th>
+                    </tr>
+                    <tr>
+                        <td class="tg-2mjt" rowspan="4">SUNDAY</td>
+                        <td class="tg-cmwg">Service Intercession</td>
+                        <td class="tg-cmwg">7:00-8:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg">First Service</td>
+                        <td class="tg-cmwg">8:00-10:15am</td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg">Second Service</td>
+                        <td class="tg-cmwg">10:20-1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg">Children's Church</td>
+                        <td class="tg-cmwg">12:00-1:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg">Monday-Friday</td>
+                        <td class="tg-cmwg">Morning Devotion</td>
+                        <td class="tg-cmwg">6:00-7:00am</td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg">Every 3rd Friday of the <br> month</td>
+                        <td class="tg-cmwg">KESHA</td>
+                        <td class="tg-cmwg"></td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg">Saturday</td>
+                        <td class="tg-cmwg">Choir reharsals and<br>prayers</td>
+                        <td class="tg-cmwg">3:00pm</td>
+                    </tr>
+                    <tr>
+                        <td class="tg-cmwg" colspan="3">Home Churches Different days during the week</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!--end grid-right-->
+    <div class="clear"> </div>
+</div>
