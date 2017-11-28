a1e35084e6a7cb9c5ea37e289cc2156ba4cab764
hack3r <amshelkanyi@gmail.com>
2017-11-28 21:05:59 +0300

    verify usage of classes and ids

--- header.php
+++ header.php
@@ -1,58 +1,56 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Shekinah Pentecostal church</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/animate.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/camera.css" rel="stylesheet" type="text/css" media="all" />
+        <!-- jQuery -->
+        <!--start slider-->
+        <!--<script type="text/javascript" src="js/jquery.js"></script>-->
+        <script src="js/jquery.min.js"></script>
+        <script type='text/javascript' src="js/jquery.mobile.customized.min.js"></script>
+        <script type='text/javascript' src="js/jquery.easing.1.3.js"></script>
+        <script type='text/javascript' src="js/camera.min.js"></script>
+        <script type="text/javascript" src="js/responsive-nav.js"></script>
+    </head>
 
-<head>
-	<title>Shekinah Pentecostal church</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all" />
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<!-- jQuery -->
-	<!--start slider-->
-	<link href="css/camera.css" rel="stylesheet" type="text/css" media="all" />
-	<script src="js/jquery.min.js"></script>
-	<script type='text/javascript' src="js/jquery.mobile.customized.min.js"></script>
-	<script type='text/javascript' src="js/jquery.easing.1.3.js"></script>
-	<script type='text/javascript' src="js/camera.min.js"></script>
-	<script type="text/javascript" src="js/responsive-nav.js"></script>
+    <body>
+        <!--star-wrap-->
+        <div class="wrap">
+            <!--header-->
+            <div class="header">
+                <!--start-logo-->
+                <div class="logo">
+                    <div id="img">
+                        <img src="logo.jpg">
+                    </div>
+                    <a href="index.html">
+                        <h1>
+                            <span class="animated zoomIn" />SHEKINAH PENTECOSTAL CHURCH</h1>
+                    </a>
+                </div>
+                <!--end-logo-->
+                <!--start-menu-->
+                <nav class="ui menu">
+                    <a class="toggleMenu" href="#"><img src="images/nav.png" alt=" " /></a>
+                    <ul class="nav">
+                        <div class="animated slideInRight" />
+                        <li class="active"><a href="index.php">home</a></li>
+                        <li><a href="pastoral.php">leadership</a></li>
+                        <li><a href="blog.php">blog</a></li>
+                        <li><a href="gallery.php">gallery</a></li>
+                        <li><a href="contact.php">contact us</a></li>
+                        <div class="clear"> </div>
+                    </ul>
+                </nav>
+                <div class="clear"> </div>
+                <!--end-menu-->
 
-</head>
-
-<body>
-	<!--star-wrap-->
-	<div class="wrap">
-		<!--header-->
-		<div class="header">
-			<!--start-logo-->
-			<div class="logo">
-				<div id="img">
-					<img src="logo.jpg">
-				</div>
-				<a href="index.html">
-					<h1>
-						<div class="animated zoomIn" />SHEKINAH PENTECOSTAL CHURCH</h1>
-				</a>
-			</div>
-			<!--end-logo-->
-			<!--start-menu-->
-			<nav class="ui menu">
-				<a class="toggleMenu" href="#"><img src="images/nav.png" alt=" " /></a>
-				<ul class="nav">
-					<div class="animated slideInRight" />
-					<li class="active"><a href="index.html">home</a></li>
-					<li><a href="Pastoralteam.html">leadership</a></li>
-					<li><a href="blog.html">blog</a></li>
-					<li><a href="portfolio.html">gallery</a></li>
-					<li><a href="contact.html">contact us</a></li>
-					<div class="clear"> </div>
-				</ul>
-			</nav>
-			<div class="clear"> </div>
-			<!--end-menu-->
-
-		</div>
-	</div>
-	<!--end-header-->
-	<!--end wrap-->
+            </div>
+        </div>
+        <!--end-header-->
+        <!--end wrap-->
