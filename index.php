4d7d7001513a9d326d33daa75c7905e67845cd74
hack3r <amshelkanyi@gmail.com>
2017-11-28 19:47:34 +0300

    beautify code

--- index.php
+++ index.php
@@ -1,188 +1,185 @@
 <?php
-
 include "header.php";
 ?>
 <!-- start slider -->
-	<div class="main-con">
-		<div class="slider">
-			<div class="animated fadeInUpBig" />
-			<div class="color"> <span></span></div>
-			<div class="wrap">
-				<div class="fluid_container">
-					<div class="camera_wrap camera_magenta_skin" id="camera_wrap_2">
-						<div data-thumb="images/them1.png" data-src="images/SAM_5280.JPG">
-							<div class="camera_caption fadeFromBottom">
-								<h2>SERVICE</h2>
-								<p>Revival voices</p>
-							</div>
-						</div>
-						<div data-thumb="images/them3.JPG" data-src="images/SAM_3317.JPG">
-							<div class="camera_caption fadeFromBottom">
-								<h2>TAKE IT BY FORCE</h2>
-								<p>Powerful confrence </p>
-							</div>
-						</div>
-						<div data-thumb="images/them4.JPG" data-src="images/SAM_3395.JPG">
-							<div class="camera_caption fadeFromBottom">
-								<h2>Shekinah Choir</h2>
-								<p>Marvelous voices singing praise and worship </p>
-							</div>
-						</div>
-						<div data-thumb="images/them5.JPG" data-src="images/SAM_3512.JPG">
-							<div class="camera_caption fadeFromBottom">
-								<h2>SUNDAY SCHOOL</h2>
-								<p>Even the Kids are brought up praising and worshipping God.</p>
-							</div>
-						</div>
-						<div data-thumb="images/them6.JPG" data-src="images/SAM_3513.JPG">
-							<div class="camera_caption fadeFromBottom">
-								<h2>YOUTH</h2>
-								<p>Youth group of young and talented minds.</p>
-							</div>
-						</div>
+<div class="main-con">
+    <div class="slider">
+        <div class="animated fadeInUpBig" />
+        <div class="color"> <span></span></div>
+        <div class="wrap">
+            <div class="fluid_container">
+                <div class="camera_wrap camera_magenta_skin" id="camera_wrap_2">
+                    <div data-thumb="images/them1.png" data-src="images/SAM_5280.JPG">
+                        <div class="camera_caption fadeFromBottom">
+                            <h2>SERVICE</h2>
+                            <p>Revival voices</p>
+                        </div>
+                    </div>
+                    <div data-thumb="images/them3.JPG" data-src="images/SAM_3317.JPG">
+                        <div class="camera_caption fadeFromBottom">
+                            <h2>TAKE IT BY FORCE</h2>
+                            <p>Powerful confrence </p>
+                        </div>
+                    </div>
+                    <div data-thumb="images/them4.JPG" data-src="images/SAM_3395.JPG">
+                        <div class="camera_caption fadeFromBottom">
+                            <h2>Shekinah Choir</h2>
+                            <p>Marvelous voices singing praise and worship </p>
+                        </div>
+                    </div>
+                    <div data-thumb="images/them5.JPG" data-src="images/SAM_3512.JPG">
+                        <div class="camera_caption fadeFromBottom">
+                            <h2>SUNDAY SCHOOL</h2>
+                            <p>Even the Kids are brought up praising and worshipping God.</p>
+                        </div>
+                    </div>
+                    <div data-thumb="images/them6.JPG" data-src="images/SAM_3513.JPG">
+                        <div class="camera_caption fadeFromBottom">
+                            <h2>YOUTH</h2>
+                            <p>Youth group of young and talented minds.</p>
+                        </div>
+                    </div>
 
-					</div>
-					<!-- #camera_wrap_2 -->
-				</div>
-				<!-- .fluid_container -->
-				<div class="clear"></div>
-			</div>
-		</div>
-		<!--end slider-->
-		<!--start home-page-con-->
+                </div>
+                <!-- #camera_wrap_2 -->
+            </div>
+            <!-- .fluid_container -->
+            <div class="clear"></div>
+        </div>
+    </div>
+    <!--end slider-->
+    <!--start home-page-con-->
 
-		<div class="home-page-con">
-			<div class="wrap">
-				<div class="top-grids">
-					<div class="top-grid">
-						<div class="top-grid-info">
-							<h2>WELCOME</h2>
-							<div class="clear"> </div>
-						</div>
-						<p>Shallom and welcome to ou website. We endevour to forward God's word to every corner of the world and emphasize on fellowship, discipleship, worship, ministry and evangelism . </p>
-					</div>
-					<div class="top-grid">
-						<div class="top-grid-info">
-							<h2>VISION</h2>
-							<div class="clear"> </div>
-						</div>
-						<p>To be a christ-centered Bible teaching church dedicated to helping develop the spiritual growth of each member of the family and equip them to become active in the ministry. </p>
-					</div>
-					<div class="top-grid">
-						<div class="top-grid-info">
-							<h2>MISSION</h2>
-							<div class="clear"> </div>
-						</div>
-						<p>We exist as a family of mature believers transforming seekers into disciples, developing talents and empowering people to win souls and establishing strong churches. </p>
-					</div>
-					<div class="clear"> </div>
-				</div>
+    <div class="home-page-con">
+        <div class="wrap">
+            <div class="top-grids">
+                <div class="top-grid">
+                    <div class="top-grid-info">
+                        <h2>WELCOME</h2>
+                        <div class="clear"> </div>
+                    </div>
+                    <p>Shallom and welcome to ou website. We endevour to forward God's word to every corner of the world and emphasize on fellowship, discipleship, worship, ministry and evangelism . </p>
+                </div>
+                <div class="top-grid">
+                    <div class="top-grid-info">
+                        <h2>VISION</h2>
+                        <div class="clear"> </div>
+                    </div>
+                    <p>To be a christ-centered Bible teaching church dedicated to helping develop the spiritual growth of each member of the family and equip them to become active in the ministry. </p>
+                </div>
+                <div class="top-grid">
+                    <div class="top-grid-info">
+                        <h2>MISSION</h2>
+                        <div class="clear"> </div>
+                    </div>
+                    <p>We exist as a family of mature believers transforming seekers into disciples, developing talents and empowering people to win souls and establishing strong churches. </p>
+                </div>
+                <div class="clear"> </div>
+            </div>
 
-				<div class="heading">
-					<h2>recent works</h2>
-					<h6> </h6>
-					<div class="clear"> </div>
-				</div>
+            <div class="heading">
+                <h2>recent works</h2>
+                <h6> </h6>
+                <div class="clear"> </div>
+            </div>
 
+            <!--end home-page-con-->
+            <div class="main_btm">
 
+                <!--start-mfp -->
+                <div id="small-dialog1" class="mfp-hide">
+                    <div class="pop_up">
+                        <h2>Helping the Needy</h2>
+                        <img src="images/IMG_4801.JPG" alt=" " />
+                        <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                    </div>
+                </div>
+                <div class="main_btm">
+                    <div id="small-dialog2" class="mfp-hide">
+                        <div class="pop_up">
+                            <h2>RUIRU MISSION</h2>
+                            <img src="images/ruiru/SAM_3212.JPG" alt=" " />
+                            <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                        </div>
+                    </div>
+                    <div class="main_btm">
+                        <div id="small-dialog3" class="mfp-hide">
+                            <div class="pop_up">
+                                <h2>TAKE IT BY FORCE</h2>
+                                <img src="images/SAM_3317.JPG" alt=" " />
+                                <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            </div>
+                        </div>
+                        <div class="main_btm">
+                            <div id="small-dialog4" class="mfp-hide">
+                                <div class="pop_up">
+                                    <h2>GOOD SAMARITAN</h2>
+                                    <img src="images/SAM_3287.JPG" alt=" " />
+                                    <p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                                </div>
+                            </div>
+                            <!--end-mfp -->
+                            <!--start-content-->
+                            <div class="gallery">
+                                <div class="clear"> </div>
+                                <div class="container">
 
+                                    <div id="gallerylist">
 
-				<!--end home-page-con-->
-				<div class="main_btm">
+                                        <div class="gallerylist-wrapper">
+                                            <a class="popup-with-zoom-anim" href="#small-dialog1">
+                                                <img src="images/IMG_4801.JPG" alt="Image 1" />
+                                                <span><img src="images/plus.png" alt=" "/> </span>
+                                                <div class="desc">
+                                                    <h2>Helping the needy</h2>
+                                                    <h3>june 15,2014</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="gallerylist-wrapper">
+                                            <a class="popup-with-zoom-anim" href="#small-dialog2">
+                                                <img src="images/ruiru/SAM_3212.JPG" alt="Image 2" />
+                                                <span><img src="images/plus.png" alt=" "/> </span>
+                                                <div class="desc">
+                                                    <h2>RUIRU MISSION</h2>
+                                                    <h3>june 15,2014</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="gallerylist-wrapper">
+                                            <a class="popup-with-zoom-anim" href="#small-dialog3">
+                                                <img src="images/SAM_3317.JPG" alt="SAM_3317" />
+                                                <span><img src="images/plus.png" alt=" "/> </span>
+                                                <div class="desc">
+                                                    <h2>TAKE IT BY FORCE</h2>
+                                                    <h3>june 15,2014</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="gallerylist-wrapper">
+                                            <a class="popup-with-zoom-anim" href="#small-dialog4">
+                                                <img src="images/SAM_3287.JPG" alt="SAM_3389" />
+                                                <span><img src="images/plus.png" alt=" "/> </span>
+                                                <div class="desc">
+                                                    <h2>GOOD SAMARITAN</h2>
+                                                    <h3>june 15,2014</h3>
+                                                </div>
+                                            </a>
+                                        </div>
 
-					<!--start-mfp -->
-					<div id="small-dialog1" class="mfp-hide">
-						<div class="pop_up">
-							<h2>Helping the Needy</h2>
-							<img src="images/IMG_4801.JPG" alt=" " />
-							<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-						</div>
-					</div>
-					<div class="main_btm">
-						<div id="small-dialog2" class="mfp-hide">
-							<div class="pop_up">
-								<h2>RUIRU MISSION</h2>
-								<img src="images/ruiru/SAM_3212.JPG" alt=" " />
-								<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-							</div>
-						</div>
-						<div class="main_btm">
-							<div id="small-dialog3" class="mfp-hide">
-								<div class="pop_up">
-									<h2>TAKE IT BY FORCE</h2>
-									<img src="images/SAM_3317.JPG" alt=" " />
-									<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-								</div>
-							</div>
-							<div class="main_btm">
-								<div id="small-dialog4" class="mfp-hide">
-									<div class="pop_up">
-										<h2>GOOD SAMARITAN</h2>
-										<img src="images/SAM_3287.JPG" alt=" " />
-										<p class="para">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-									</div>
-								</div>
-								<!--end-mfp -->
-								<!--start-content-->
-								<div class="gallery">
-									<div class="clear"> </div>
-									<div class="container">
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- container -->
+                            <script type="text/javascript" src="js/fliplightbox.min.js"></script>
+                            <script type="text/javascript">
+                                $('body').flipLightBox()
+                            </script>
+                            <div class="clear"> </div>
+                        </div>
 
-										<div id="gallerylist">
-
-											<div class="gallerylist-wrapper">
-												<a class="popup-with-zoom-anim" href="#small-dialog1">
-													<img src="images/IMG_4801.JPG" alt="Image 1" />
-													<span><img src="images/plus.png" alt=" "/> </span>
-													<div class="desc">
-														<h2>Helping the needy</h2>
-														<h3>june 15,2014</h3>
-													</div>
-												</a>
-											</div>
-											<div class="gallerylist-wrapper">
-												<a class="popup-with-zoom-anim" href="#small-dialog2">
-													<img src="images/ruiru/SAM_3212.JPG" alt="Image 2" />
-													<span><img src="images/plus.png" alt=" "/> </span>
-													<div class="desc">
-														<h2>RUIRU MISSION</h2>
-														<h3>june 15,2014</h3>
-													</div>
-												</a>
-											</div>
-											<div class="gallerylist-wrapper">
-												<a class="popup-with-zoom-anim" href="#small-dialog3">
-													<img src="images/SAM_3317.JPG" alt="SAM_3317" />
-													<span><img src="images/plus.png" alt=" "/> </span>
-													<div class="desc">
-														<h2>TAKE IT BY FORCE</h2>
-														<h3>june 15,2014</h3>
-													</div>
-												</a>
-											</div>
-											<div class="gallerylist-wrapper">
-												<a class="popup-with-zoom-anim" href="#small-dialog4">
-													<img src="images/SAM_3287.JPG" alt="SAM_3389" />
-													<span><img src="images/plus.png" alt=" "/> </span>
-													<div class="desc">
-														<h2>GOOD SAMARITAN</h2>
-														<h3>june 15,2014</h3>
-													</div>
-												</a>
-											</div>
-
-										</div>
-									</div>
-								</div>
-								<!-- container -->
-								<script type="text/javascript" src="js/fliplightbox.min.js"></script>
-								<script type="text/javascript">
-									$('body').flipLightBox()
-								</script>
-								<div class="clear"> </div>
-							</div>
-
-						</div>
-					</div>
-<?php
-include "footer.php";
+                    </div>
+                </div>
+                <?php
+                include "footer.php";
+                
