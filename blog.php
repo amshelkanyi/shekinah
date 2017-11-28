258762356647fc4c44f7830c40b5d698d71ba404
hack3r <amshelkanyi@gmail.com>
2017-11-28 21:03:32 +0300

    add include files switched from static html to dynamic pages served from server

---
+++ blog.php
@@ -0,0 +1,121 @@
+<?php
+include 'header.php';
+?>
+<!--start blog-->
+<div class="blog-content">
+    <div class="wrap">
+        <!--blog-grid-->
+        <div class="blog-grids">
+            <!-- blog-left-->
+            <div class="blog-left">
+                <!-- start slider -->
+                <div class="slider">
+                    <div class="icon">
+                        <a href="#"><span> </span></a>
+                    </div>
+                    <div class="fluid_container">
+                        <div class="camera_wrap camera_magenta_skin" id="camera_wrap_2">
+                            <div data-src="images/slider1.jpg"> </div>
+                            <div data-src="images/slider2.jpg"></div>
+                            <div data-src="images/slider3.jpg"></div>
+                            <div data-src="images/slider4.jpg"></div>
+                            <div data-src="images/slider2.jpg"></div>
+                            <div  data-src="images/slider6.jpg"></div>
+                        </div><!-- #camera_wrap_2 -->
+                    </div><!-- .fluid_container -->
+                    <div class="clear"> </div>
+                    <div class="sinfo">
+                        <div class="sinfo-left">
+                            <h3>date</h3>
+                            <span>April 15,2012</span>
+                            <h3 class="hover">tags</h3>
+                            <span><a href="#"><i>Anger</i> <i>Devil</i></a></span>
+                            <h3>comments</h3>
+                            <span class="hover1"><a href="#">4</a></span>
+                        </div>
+                        <div class="sinfo-right">
+                            <h4><a href="#">ANGER GIVES THE DEVIL A FOOTHOLD</a></h4>
+                            <p>A friend arriving for a visit was furious after driving through town. He was cursing the bus driver that smashed the bumper on his car. He was convinced the bus driver was guilt. The bus was moving down the middle of the road, overtaking a line of cars, before it hit him. To make things worse, the police told him he had to pay a fine as if he were the one who had broken the law..</p>
+                            <p style="padding:3px 0px">We’ve all had one of those days when we go those days when we go home seething with anger. When your turn comes to survive the experience remember the words of St Paul in the letter to the Ephesians:” if you are angry, you must not sin: never let the sun set on your anger or else you will give the devil a foothold. “[Ephesians 4:26-27]. Note the words used by the apostle. He does not say that all anger is necessarily sinful. After all, Jesus himself, when defending the security of “his father`s house,” made a whip of some cords and drove the sheep and cattle out of the temple. [John 2:15]</p>
+                        </div>
+                        <div class="clear"> </div>
+                    </div>
+                </div>
+
+                <h6> </h6>
+                <div class="jesus">
+                    <div class="icon1">
+                        <a href="#"><span> </span></a>
+                    </div>
+                    <div class="sinfo">
+                        <div class="sinfo-left">
+                            <h3>date</h3>
+                            <span>April 15,2012</span>
+                            <h3 class="hover">tags</h3>
+                            <span><a href="#"><i>Website</i> <i>Desings</i></a></span>
+                            <h3>comments</h3>
+                            <span class="hover1"><a href="#">4</a></span>
+                        </div>
+                        <div class="sinfo-right">
+                            <h4><a href="#">GOOD AND HOLY DEEDS</a></h4>
+                            <p>There are times when we ought to be angry. For instance, when our neighbor is being abused, it is a good and holy deed to come to his aid. God himself speaks of getting angry: “vengeance is mine I will repay ….For the lord God will see his people vindicate. He will take pity on his servants. “Deuteronomy 32:35; Psalms 135:14]There are times when we ought to be angry. For instance, when our neighbor is being abused, it is a good and holy deed to come to his aid. God himself speaks of getting angry: “vengeance is mine I will repay ….For the lord God will see his people vindicate. He will take pity on his servants. “Deuteronomy 32:35; Psalms 135:14].</p>
+                            <p style="padding:3px 0px"> The bible urges us to be filled with peace. Jesus said the children of God would be known as peacemakers [Romans 12:18; Mathew 5:9]. But he did not preach pacifism, which is often just an excuse for cowardice or laziness. It is a question of proper balance.   .</p>
+                            <p style="padding:3px 0px">Many people become filled with anger when it is in their own best interest to remain calm. Take, for instance, that traffic accident I mentioned earlier.<br/>
+                                There are many things you cannot change no matter how angry you get. There are some cases where your anger incites another person`s anger. You both race down-hill` heaping up words of rage like stones and dirt gathered by landslide. As a wise man once explained, this generates lots of heat and no light.<br/>
+                                The worst anger tends to erupt between people who know each other well. The deepest wounds left by such verbal battles are inflicted by a parent speaking to children, spouse speaking to spouse, or brothers and sisters quarrelling with each other.<br/>
+                                At the very least even when you have to get angry, take Paul`s advice seriously. Make sure that you replace today`s anger with tomorrow`s smile. It may be a cliché , [common saying] but it`s worth repeating: a gram of sugar can do more good than a tub of vinegar. God bless you.
+
+                            </p>
+                        </div>
+                        <div class="clear"> </div>
+                    </div>
+                </div>
+
+                <h6> </h6>
+                <div class="vedieo">
+                    <div class="icon1 icon2">
+                        <a href="#"><span> </span></a>
+                    </div>
+                    <div class="sinfo">
+                        <div class="sinfo-left">
+                            <h3>date</h3>
+                            <span>April 15,2012</span>
+                            <h3 class="hover">tags</h3>
+                            <span><a href="#"><i>Website</i> <i>Desings</i></a></span>
+                            <h3>comments</h3>
+                            <span class="hover1"><a href="#">4</a></span>
+                        </div>
+                        <div class="sinfo-right">
+                            <h4><a href="#">	THERE IS TIME FOR EVERYTHING(Part 7)by pastor Geoffrey Ndilai</a></h4>
+                            <p>Ecclesiastes 3: 1-8                                                                                                                                                                 A time to cast away stones: Metaphysically stones stand for truth and knowledge . Why would we want to cast truth or knowledge away? Sometimes in the cycles of life it is  to stop learning and start practicing . There are times to stop learning and start practicing. There are times to stop the study and to assimilate what have learned; to integrate it. There is a time to cast the stones away.<br/>
+                                Then the preacher goes on to say there is time to embrace. Embrace , of course, means not only the physical thing that we like to do, but it means to open to , and to incorporate higher truths into our understanding;  to welcome new opportunities and new understandings. But it is still great to do the good old fashioned embrace.<br/>
+                                Then there is a time to refrain from embracing; to not accept what is appropriate. Some of the things we have embraced into our lives require a second look and we discover it is time to give them up because they are not for our highest good at that time in our life.
+                            </p>
+                            <p style="padding:3px 0px">There is a time to lose. In the cycles of life there are times of diminishing activity. The entire universe is set up in these cycles. Scientists talk about the big bang theory.  What is going to happen as soon as the physical universe is extended as far as it can go? It will begin to contract. We are talking about eons of time. You and I don't need to worry about it. It will come to pass and the process will be repeated. That is also a very Eastern concept - Krishna speaks of breathing and in and breathing out all of creation. There is also a time to meditate; to turn inward, and then to come back out into the busyness of life. We pay our bills; we live out our lives; we raise our families. But there will always be a time of turning inward. It also means to lose outdated beliefs and prejudices.<br/>
+                                .</p>
+                        </div>
+                        <div class="clear"> </div>
+                    </div>
+
+                </div>
+                <h6> </h6>
+                <div class="text-2">
+                    <div class="icon1 icon4">
+                        <a href="#"><span> </span></a>
+                    </div>
+                    <h4><a href="#">THERE IS A TIME FOR EVERYTHING(Part 9) by pastor Geoffrey Ndilai</a></h4>
+                    <span>ECCLESIASTES 3:1-8</span>
+                    <p>A time to keep silent:  To be still. In psalms it say " Be still and know  I am GOD  "(Psalms 46:10) Keepings silences and meditating.<br/>
+                        A time to speak :Jesus said, "what I tell you in dark,(inner promptings) tell it in the daylight . What you hear in your ears ,preach it on the house tops. "(Matthew 10:27) What understanding God gives us within our own mind and mind and heart must be shared with those who have not yet been exposed to the same light we may have received .<br/>
+                        A time to love .Paul said it so beautifully in 1 Corinthians ,chapter 13:Though I speak with the tongues of men and angels ,and have not love in my heart , I am become as sounding brass or a tinkling cymbal  And though I have the gift of prophecy ,and understand all mysteries and all knowledge ;and though I have all faith ,so that I could remove mountains ,and have not love in my heart ,I am nothing .And though I Bestow all my goods to feed the poor ,and though I give my body to be burned and have not love in my heart , I gain nothing .
+                    <p style="padding:3px 0px">
+                        Love is long-suffering and kind :love does not envy :love does not make a vain display of itself, and does not boast,
+                        Does not behave itself unseemly, seeks not its own, is not easily provoked, thinks no evil, rejoices in the truth; Bears all things, hopes all things, endures all things. <br/>
+                        .</p>
+                </div>
+                <!--end slider-->
+            </div>
+            <?php
+            include 'sidebar.php';
+            include 'footer.php';
+            
---
+++ pastoral.php
@@ -0,0 +1,86 @@
+<?php
+include 'header.php';
+?>
+<div class="top-heading">
+    <!--start-wrap-->
+    <div class="wrap">
+        <h1>PASTORAL TEAM</h1>
+    </div>
+    <!--end wrap-->
+</div>
+<!--about-content-->
+<div class="wrap">
+    <div class="content">
+        <!--about-grids-->
+        <div class="main-grid">
+            <!--grid-left-->
+            <div class="grid-left">
+                <div class="animated zoomIn"/>
+                <h2 class="ui-header">BISHOP CHARLES MELIYIO</h2>
+                <p>A new chapter was opened in my life.I experienced a new orientation altogether.
+                    My priorities were changed as well.I developed a strong desire for God's word.
+                    I chose to join a christian Union at school.I encountered very serious and mature christians.</p>
+                <!--grid-left-img-->
+                <div class="grid-left-img">
+                    <i>
+                        <img src="images/index4.jpeg" alt= " " />
+                    </i>
+                    <p>This was a divine connection since they mentored me and after a short time they started giving me som
+                        responsibilities.I became an official of the christian union. When I went home during school holidays,
+                        I joined a local pentecostal church and that again opened my understanding about the things of the spirit.
+                        I joined the evangelistic team within the church and started sharing about my experience in christ during
+                        open air meetings.At this,as a result of listening to the powerful preaching of God's word,I began to
+                        understand the will and purpose of God in my life God gave me favour at our local church and was appointed
+                        to serve in different positions.</div>
+                <div class="grid-left">
+
+
+                    <br/><p/>This exposure gave me an opportunity to understand ministry.When I joined
+                    college,the same sequence of events took place.Egerton university had a very elaborate and orderly system
+                    of worship and to my surprise was appointed Pastor.Ibenefited alot from occassional pastoral training and
+                    a popular program known as 'BEST-P' where I learned the bible expository skills.It was during this time
+                    that a teaching ministry was born in me.I love teaching God's word.</p></div>
+                <div class="grid-right-img">
+                    <i>
+                        <img src="images/index3" alt= " " />
+                    </i>
+                </div>
+                <p>After working with the ministry of environment and natural resources,General motors and finally Multimedia
+                    university college I had the call of God in 1993 while praying and fasting in Kandisi plains,Ongata Rongai.
+                    The same calling would be confirmed two years later when a team from Uganda came to minister in our church.
+                    My pastor then Bishop Sammy Yole Makau prayed for us and committed us to start God's work .From humble
+                    begining of a small rented room, the church grew in leaps and bounds and on 23rd November 1997
+                    Bishop Dr.Robert Mdzomba ordained me as a pastor and seven years later appointed Nairobi region overseer
+                    in char I am blessed with a wonderful wife,Nancy Meliyio and two children Perez Lomunyak and Zilpah Nashipai<br/></p>
+
+                <div class="clear"> </div>
+                <div class="grid-left" >
+                    <h2 class="ui-header">PASTOR LEMALOI OLE NDILAI</h2>
+                    <div class="grid-left-img">
+                        <i>
+                            <img src="images/index.jpeg" alt= " " />
+                        </i>
+                    </div>
+                    <div class="container-left">
+                        <p>I got born again in February 1997 while still a student at high school.
+                            Initially, i was a catholic through the influence of my older sister who is still a practising catholic
+                            One day as i was reading a certain Christian book, i deeply sensed in my spirit that i should give my life
+                            to christ. Without hesitation i went to the christian union chairman and told him i want to give my life
+                            to Christ. From there began my journey to salvation. i have served in various positions in the church since
+                            high school and currently im a youth pastor with a great zeal for the Youth Ministry.
+                            What lead me to Christ was that yearning of being used powerfully by God in the Great Commission and
+                            taking the Gospel to all corners of the world and through the power of God deliver those who are oppressed
+                            from the yoke of bondage.</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+<?php
+            include 'sidebar.php';
+?>
+        </div>
+    </div>
+</div>
+<?php
+include 'footer.php';
